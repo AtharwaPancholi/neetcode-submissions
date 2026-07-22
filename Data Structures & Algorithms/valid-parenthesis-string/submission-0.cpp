a2d60beeeedcb9f0d1b9ec63e400095544514ef9
AtharwaPancholi <83603125+AtharwaPancholi@users.noreply.github.com>
2026-07-22 22:55:45 +0530

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int minOpen=0;
+        int maxOpen=0;
+        for(char ch: s){
+            if(ch=='('){
+                minOpen++;
+                maxOpen++;
+            }
+            else if(ch==')'){
+                minOpen--;
+                maxOpen--;
+            }
+            else{
+                minOpen--;
+                maxOpen++;
+            }
+            if(maxOpen<0) return false;
+            minOpen=max(minOpen,0);
+        }
+        return minOpen==0;
+    }
+};
